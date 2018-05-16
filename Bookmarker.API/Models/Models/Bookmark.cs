43735330391b5c4d4a3bf5636928bccbf15a2281
Timothy Hayes <tjhayes011@gmail.com>
2018-05-16 13:29:34 -0400

    Add properties and constructors to models.

--- Bookmarker.API/Models/Models/Bookmark.cs
+++ Bookmarker.API/Models/Models/Bookmark.cs
@@ -4,5 +4,17 @@ namespace Models.Models
 {
     public class Bookmark : ABaseEntity
     {
+        public Bookmark(string name, Collection collection, string url)
+        {
+            Name = name;
+            Collection = collection;
+            URL = url;
+            Rating = 0;
+        }
+
+        public string Name { get; set; }
+        public virtual Collection Collection { get; set; }
+        public string URL { get; set; }
+        public int Rating { get; set; }
     }
 }
--- Bookmarker.API/Models/Models/Collection.cs
+++ Bookmarker.API/Models/Models/Collection.cs
@@ -1,8 +1,26 @@
 ﻿using Models.Abstracts;
+using System;
+using System.Collections.Generic;
 
 namespace Models.Models
 {
     public class Collection : ABaseEntity
     {
+        public Collection(string name, string description, User owner)
+        {
+            Name = name;
+            Description = description;
+            Private = true;
+            Rating = 0;
+            Owner = owner;
+            Bookmarks = new HashSet<Bookmark>();
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Private { get; set; }
+        public int Rating { get; set; }
+        public virtual User Owner { get; set; }
+        public virtual ICollection<Bookmark> Bookmarks { get; set; }
     }
 }
