9be2ac4b4e1ed48a5de88860a212569bcc849f09
Timothy Hayes <tjhayes011@gmail.com>
2018-05-17 16:59:15 -0400

    Add Table tests for Collection and Bookmark

--- Bookmarker.API/Bookmarker.Test/TestBookmarkerTestContext.cs
+++ Bookmarker.API/Bookmarker.Test/TestBookmarkerTestContext.cs
@@ -32,5 +32,37 @@ namespace Bookmarker.Test
             // Assert
             Assert.AreEqual(expectedName, actualName);
         }
+
+        [TestMethod]
+        public void TestCollectionTable()
+        {
+            // Arrange
+            Repository<Collection> collections = new Repository<Collection>(_testContext);
+            string expectedName = "c#";
+
+            // Act
+            IEnumerator<Collection> collEnum = collections.Table.GetEnumerator();
+            collEnum.MoveNext();
+            string actualName = collEnum.Current.Name;
+
+            // Assert
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [TestMethod]
+        public void TestBookmarkTable()
+        {
+            // Arrange
+            Repository<Bookmark> bookmarks = new Repository<Bookmark>(_testContext);
+            string expectedName = "c# intro";
+
+            // Act
+            IEnumerator<Bookmark> bmEnum = bookmarks.Table.GetEnumerator();
+            bmEnum.MoveNext();
+            string actualName = bmEnum.Current.Name;
+
+            // Assert
+            Assert.AreEqual(expectedName, actualName);
+        }
     }
 }
