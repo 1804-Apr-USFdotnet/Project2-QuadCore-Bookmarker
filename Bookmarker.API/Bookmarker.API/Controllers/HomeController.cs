164e4c5186709035a675c3074ba523ea808c9516
Timothy Hayes <tjhayes011@gmail.com>
2018-05-18 08:58:57 -0400

    complete feature: get all users via test context and API, server-side via IHttpActionResult and client-side via ajax

--- Bookmarker.API/Bookmarker.API/Controllers/HomeController.cs
+++ Bookmarker.API/Bookmarker.API/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Bookmarker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +12,27 @@ namespace Bookmarker.API.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly HttpClient httpClient = new HttpClient();
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        public async Task<ActionResult> Users()
+        {
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+            HttpResponseMessage response = await httpClient.GetAsync("http://localhost:55287/api/Users");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return View("Error");
+            }
+
+            var users = await response.Content.ReadAsAsync<IEnumerable<User>>();
+            return View(users);
         }
     }
 }
