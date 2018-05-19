c3b352ac74395463d4fffe0934275d099feff027
Timothy Hayes <tjhayes011@gmail.com>
2018-05-19 13:20:20 -0400

    Add test for UsersAPI post

--- Bookmarker.API/Bookmarker.Test/TestUsersApiController.cs
+++ Bookmarker.API/Bookmarker.Test/TestUsersApiController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -37,6 +38,8 @@ namespace Bookmarker.Test
             {
                 actualUserCount++;
             }
+
+            // Assert
             Assert.AreEqual(expectedUserCount, actualUserCount);
         }
 
@@ -54,6 +57,67 @@ namespace Bookmarker.Test
 
             // Assert
             Assert.AreEqual(expectedUsername, user.Username);
+        }
+
+        [TestMethod]
+        public async Task TestUsersAPIPostAsync()
+        {
+            // Should be 2 users to begin with
+
+            // Arrange
+            int expectedUserCount = 2;
+            int actualUserCount = 0;
+
+            // Act
+            IHttpActionResult userResult = controller.Get();
+            var message = await userResult.ExecuteAsync(new System.Threading.CancellationToken());
+            var users = await message.Content.ReadAsAsync<IEnumerable<User>>();
+            foreach (var user in users)
+            {
+                actualUserCount++;
+            }
+
+            // Assert
+            Assert.AreEqual(expectedUserCount, actualUserCount);
+
+            //////////////////////////////////////////////////////////////////
+
+            // Post -- invalid model -- expect bad request message
+            User newUser = new User();
+            newUser.Username = "jon";
+            newUser.Password = "badPw";
+            newUser.Email = "jon@mail.com";
+            newUser.Id = new Guid("55555555-4444-4444-4444-222222222222");
+            newUser.Entity
+
+            IHttpActionResult result = controller.Post(newUser);
+            var badPostMessage = await result.ExecuteAsync(new System.Threading.CancellationToken());
+            Assert.AreEqual(HttpStatusCode.BadRequest, badPostMessage.StatusCode);
+
+            newUser.Password = "okayPassword";
+            IHttpActionResult goodResult = controller.Post(newUser);
+            var goodPostMessage = await goodResult.ExecuteAsync(new System.Threading.CancellationToken());
+            Assert.AreEqual(HttpStatusCode.OK, goodPostMessage.StatusCode);
+
+            /////////////////////////////////////////////////////////////////
+
+            // Now 3 users instead of 2
+
+            // Arrange
+            expectedUserCount = 3;
+            actualUserCount = 0;
+
+            // Act
+            userResult = controller.Get();
+            message = await userResult.ExecuteAsync(new System.Threading.CancellationToken());
+            users = await message.Content.ReadAsAsync<IEnumerable<User>>();
+            foreach (var user in users)
+            {
+                actualUserCount++;
+            }
+
+            // Assert
+            Assert.AreEqual(expectedUserCount, actualUserCount);
         }
     }
 }
