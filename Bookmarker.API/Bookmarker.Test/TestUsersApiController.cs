b3d8cc7389f7772d9e8567e38d8cf756427866ef
Timothy Hayes <tjhayes011@gmail.com>
2018-05-19 12:28:42 -0400

    Clean up test for UsersAPI Get

--- Bookmarker.API/Bookmarker.Test/TestUsersApiController.cs
+++ Bookmarker.API/Bookmarker.Test/TestUsersApiController.cs
@@ -13,48 +13,31 @@ namespace Bookmarker.Test
     [TestClass]
     public class TestUsersApiController
     {
-        private readonly UsersController controller 
-            = new UsersController(new BookmarkerTestContext());
+        private readonly UsersController controller;
 
-        //private static readonly HttpClient httpClient = new HttpClient();
-        //private static readonly string apiPath = "http://localhost:55287/api";
+        public TestUsersApiController()
+        {
+            controller = new UsersController(new BookmarkerTestContext());
+            controller.ControllerContext.Configuration = new HttpConfiguration();
+            controller.Request = new HttpRequestMessage();
+        }
 
         [TestMethod]
         public async Task TestUsersGetAsync()
         {
             // Arrange
-            //HttpResponseMessage response = 
-            //    new HttpResponseMessage(System.Net.HttpStatusCode.InternalServerError);
-
-            //try
-            //{
-            //    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
-            //    response = await httpClient.GetAsync($"{apiPath}/Users");
-            //}
-            //catch
-            //{
-            //    Assert.Fail("API test server is not running or has incorrect path.");
-            //}
-            //int expectedCount = 6;
-            int expectedCount = 2;
-            int actualCount = 0;
+            int expectedUserCount = 2;
+            int actualUserCount = 0;
 
             // Act
-            //if (!response.IsSuccessStatusCode)
-            //{
-            //    Assert.Fail($"Get request failed: {response.StatusCode}");
-            //}
-            //var users = await response.Content.ReadAsAsync<IEnumerable<User>>();
-            controller.ControllerContext.Configuration = new HttpConfiguration();
-            controller.Request = new HttpRequestMessage();
             IHttpActionResult userResult = controller.Get();
             var message = await userResult.ExecuteAsync(new System.Threading.CancellationToken());
             var users = await message.Content.ReadAsAsync<IEnumerable<User>>();
             foreach (var user in users)
             {
-                actualCount++;
+                actualUserCount++;
             }
-            Assert.AreEqual(expectedCount, actualCount);
+            Assert.AreEqual(expectedUserCount, actualUserCount);
         }
     }
 }
