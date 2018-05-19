168daab8f341492b12e25b438c29f84b938f5247
Billy <do.billy5@gmail.com>
2018-05-19 13:42:12 -0400

    created view models

---
+++ Bookmarker.MVC/Bookmarker.MVC/Models/AccountViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookmarker.MVC.Models
+{
+    public class AccountViewModel
+    {
+        public string Guid { get; set; }
+        public string Created { get; set; }
+        public string Modified { get; set; }
+
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public IEnumerable<CollectionViewModel> Collections { get; set; }
+    }
+}
