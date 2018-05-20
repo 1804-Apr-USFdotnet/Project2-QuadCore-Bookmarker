236ff06ee777b968ade258edf8da81892fdb3d88
Billy <do.billy5@gmail.com>
2018-05-19 21:04:27 -0400

    started test controller

---
+++ Bookmarker.API/Bookmarker.Test/Controllers/UserCollectionsControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bookmarker.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmarker.API.Controllers.Tests
+{
+    [TestClass()]
+    public class UserCollectionsControllerTests
+    {
+        [TestMethod()]
+        public void UserCollectionsControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UserCollectionsControllerTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCollectionByIndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCollectionByIdTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
