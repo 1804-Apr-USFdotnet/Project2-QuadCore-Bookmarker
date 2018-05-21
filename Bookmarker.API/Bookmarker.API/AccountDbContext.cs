e17ca710eedf3676eb1ac90f2c33102f70e6eb00
Timothy Hayes <tjhayes011@gmail.com>
2018-05-21 18:32:59 -0400

    Add AccountDbContext

---
+++ Bookmarker.API/Bookmarker.API/AccountDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Bookmarker.API
+{
+    public class AccountDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AccountDbContext() : base("BookmarkerAccountDb")
+        {
+
+        }
+    }
+}
