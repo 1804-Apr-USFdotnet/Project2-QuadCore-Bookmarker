729145c36111f5bbafbe1ee5d355802a5cceae25
Timothy Hayes <tjhayes011@gmail.com>
2018-05-21 10:03:24 -0400

    Cull auto-generated MVC from API.

--- Bookmarker.API/Bookmarker.Logic/Properties/AssemblyInfo.cs
+++ Bookmarker.API/Bookmarker.Logic/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bookmarker.Services")]
+[assembly: AssemblyTitle("Bookmarker.Logic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bookmarker.Services")]
+[assembly: AssemblyProduct("Bookmarker.Logic")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d5e1f5bb-1fcb-4ed4-8029-df7aa0c43697")]
+[assembly: Guid("a2448b61-7b7d-4d70-b894-a3a5760d5db9")]
 
 // Version information for an assembly consists of the following four values:
 //
