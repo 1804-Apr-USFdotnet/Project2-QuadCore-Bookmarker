6a08b9ad0a2be57038cc57a43655cda06bc820c3
Timothy Hayes <tjhayes011@gmail.com>
2018-05-21 19:57:00 -0400

    Accounts Controller

---
+++ Bookmarker.MVC/Bookmarker.MVC/Controllers/AccountsController.cs
@@ -0,0 +1,66 @@
+﻿using Bookmarker.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bookmarker.MVC.Controllers
+{
+    public class AccountsController : AServiceController
+    {
+        // GET: Accounts/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Accounts/Login
+        [HttpPost]
+        public async Task<ActionResult> Login(AccountViewModel account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Error");
+            }
+
+            HttpRequestMessage apiRequest = CreateRequestToService(HttpMethod.Post, "api/Account/Login");
+            apiRequest.Content = new ObjectContent<AccountViewModel>(account, new JsonMediaTypeFormatter());
+
+            HttpResponseMessage apiResponse;
+            try
+            {
+                apiResponse = await HttpClient.SendAsync(apiRequest);
+            }
+            catch
+            {
+                return View("Error");
+            }
+
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                return View("Error");
+            }
+
+            PassCookiesToClient(apiResponse);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool PassCookiesToClient(HttpResponseMessage apiResponse)
+        {
+            if (apiResponse.Headers.TryGetValues("Set-Cookie", out IEnumerable<string> values))
+            {
+                foreach (string value in values)
+                {
+                    Response.Headers.Add("Set-Cookie", value);
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
