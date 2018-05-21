cb2a61bb929c5c7472fce9d2462b2d790726dc4f
Timothy Hayes <tjhayes011@gmail.com>
2018-05-21 18:39:29 -0400

    Add account class

---
+++ Bookmarker.API/Bookmarker.API/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookmarker.API.Models
+{
+    public class Account
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = 
+            "Password must be between 8 and 40 characters inclusive.")]
+        public string Password { get; set; }
+    }
+}
