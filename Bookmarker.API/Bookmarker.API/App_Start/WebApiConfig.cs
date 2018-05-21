aeeaf7cb0cd9773a62f56a6c7eeb89c47cc20bb1
Timothy Hayes <tjhayes011@gmail.com>
2018-05-21 18:19:34 -0400

    Add to WebApiConfig

--- Bookmarker.API/Bookmarker.API/App_Start/WebApiConfig.cs
+++ Bookmarker.API/Bookmarker.API/App_Start/WebApiConfig.cs
@@ -7,9 +7,14 @@ namespace Bookmarker.API
 {
     public static class WebApiConfig
     {
+        public static string AuthenticationType = "BookmarkerCookie";
+        public static string CookieName = "BookmarkerCookie";
+
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
+            config.Filters.Add(new AuthorizeAttribute());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
