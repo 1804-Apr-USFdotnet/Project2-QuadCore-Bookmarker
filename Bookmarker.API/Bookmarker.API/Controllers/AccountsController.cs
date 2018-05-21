9932b79790b34773d9141c741bb4dbde9928b869
Timothy Hayes <tjhayes011@gmail.com>
2018-05-21 18:51:22 -0400

    Add Register

--- Bookmarker.API/Bookmarker.API/Controllers/AccountsController.cs
+++ Bookmarker.API/Bookmarker.API/Controllers/AccountsController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Bookmarker.API.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,5 +13,31 @@ namespace Bookmarker.API.Controllers
 {
     public class AccountsController : ApiController
     {
+        [HttpPost]
+        [Route("~/api/Register")]
+        [AllowAnonymous]
+        public IHttpActionResult Register(Account account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // Register
+            var userStore = new UserStore<IdentityUser>(new AccountDbContext());
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var user = new IdentityUser(account.Username);
+
+            if (userManager.Users.Any(u => u.UserName == account.Username))
+            {
+                return BadRequest();
+            }
+
+            userManager.Create(user, account.Password);
+
+            // Login
+
+            return Ok();
+        }
     }
 }
