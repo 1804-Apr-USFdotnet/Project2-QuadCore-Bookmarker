6881eb70f754014b1cf93c5d925bcb68eb5b2a5f
Billy <do.billy5@gmail.com>
2018-05-21 14:39:02 -0400

    implementing tests for badrequest and notfound

--- Bookmarker.API/Bookmarker.Test/Controllers/UserCollectionsControllerTests.cs
+++ Bookmarker.API/Bookmarker.Test/Controllers/UserCollectionsControllerTests.cs
@@ -16,6 +16,10 @@ namespace Bookmarker.API.Controllers.Tests
     {
         private readonly UserCollectionsController controller;
 
+        private readonly string userGuid1 = "88888888-4444-4444-4444-222222222222";
+        private readonly string userGuid2 = "11111111-4444-4444-4444-222222222222";
+        private readonly string userGuidNotExist = "FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF";
+
         //[TestMethod()]
         public UserCollectionsControllerTests()
         {
@@ -34,7 +38,7 @@ namespace Bookmarker.API.Controllers.Tests
         public async Task GetTest()
         {
             // Arrange
-            Guid userId = new Guid("88888888-4444-4444-4444-222222222222");
+            Guid userId = new Guid(userGuid1);
             string expectedCollectionName = "c#";
             string expectedDescription = ".net framework";
             // Act
@@ -53,14 +57,42 @@ namespace Bookmarker.API.Controllers.Tests
         }
 
         [TestMethod()]
+        public async Task GetBadRequestTest()
+        {
+            // check, if userId is not in database
+            // returns BadRequest HttpActionResult
+            Guid userId = new Guid(userGuidNotExist);
+            var expectedStatusCode = System.Net.HttpStatusCode.BadRequest;
+
+            IHttpActionResult result = controller.Get(userId);
+            var response = await result.ExecuteAsync(new System.Threading.CancellationToken());
+            var actualStatusCode = response.StatusCode;
+
+            Assert.AreEqual(expectedStatusCode, actualStatusCode);
+        }
+
+        [TestMethod()]
+        public async Task GetNotFoundTest()
+        {
+            // check, if userId has no collections
+            // returns NotFound HttpActionResult
+            
+            //TODO: Add another user to test data
+            //      that has no collections
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
         public async Task GetCollectionByIndexTest()
         {
             // Arrange
-            Guid userId = new Guid("11111111-4444-4444-4444-222222222222");
+            Guid userId = new Guid(userGuid2);
+            int index = 2;
             string expectedCollectionName = "c#";
             string expectedDescription = "c# tutorials";
             // Act
-            IHttpActionResult result = controller.GetCollectionByIndex(userId, 2);
+            IHttpActionResult result = controller.GetCollectionByIndex(userId, index);
             var response = await result.ExecuteAsync(new System.Threading.CancellationToken());
             var collection = await response.Content.ReadAsAsync<Models.Collection>();
 
@@ -73,10 +105,43 @@ namespace Bookmarker.API.Controllers.Tests
         }
 
         [TestMethod()]
+        public async Task GetCollectionByIndexBadRequestTest()
+        {
+            // check, if userId is not in database
+            // returns BadRequest HttpActionResult
+            Guid userId = new Guid(userGuidNotExist);
+            int index = 1;
+            var expectedStatusCode = System.Net.HttpStatusCode.BadRequest;
+            
+
+            IHttpActionResult result = controller.GetCollectionByIndex(userId, index);
+            var response = await result.ExecuteAsync(new System.Threading.CancellationToken());
+            var actualStatusCode = response.StatusCode;
+
+            Assert.AreEqual(expectedStatusCode, actualStatusCode);
+        }
+
+        [TestMethod()]
+        public async Task GetCollectionByIndexNotFoundTest()
+        {
+            // check, if index is greater than collection count
+            // returns NotFound HttpActionResult
+            Guid userId = new Guid(userGuid2);
+            int index = 3;
+            var expectedStatusCode = System.Net.HttpStatusCode.NotFound;
+
+            IHttpActionResult result = controller.GetCollectionByIndex(userId, index);
+            var response = await result.ExecuteAsync(new System.Threading.CancellationToken());
+            var actualStatusCode = response.StatusCode;
+
+            Assert.AreEqual(expectedStatusCode, actualStatusCode);
+        }
+
+        [TestMethod()]
         public async Task GetCollectionByIdTest()
         {
             // Arrange
-            Guid userId = new       Guid("11111111-4444-4444-4444-222222222222");
+            Guid userId = new       Guid(userGuid2);
             Guid collectionId = new Guid("bbbbbbbb-4444-4444-4444-222222222222");
             string expectedCollectionName = "recipes";
             string expectedDescription = "my favorites";
@@ -91,6 +156,30 @@ namespace Bookmarker.API.Controllers.Tests
             // Assert
             Assert.AreEqual(expectedCollectionName, actualName);
             Assert.AreEqual(expectedDescription, actualDescription);
+        }
+
+        [TestMethod()]
+        public async Task GetCollectionByIdBadRequestTest()
+        {
+            // check, if userId is not in database
+            // returns BadRequest HttpActionResult
+            Guid userId = new Guid(userGuidNotExist);
+            Guid collectionId = new Guid("bbbbbbbb-4444-4444-4444-222222222222");
+            var expectedStatusCode = System.Net.HttpStatusCode.BadRequest;
+
+            IHttpActionResult result = controller.GetCollectionById(userId, collectionId);
+            var response = await result.ExecuteAsync(new System.Threading.CancellationToken());
+            var actualStatusCode = response.StatusCode;
+
+            Assert.AreEqual(expectedStatusCode, actualStatusCode);
+        }
+
+        [TestMethod()]
+        public async Task GetCollectionByIdNotFoundTest()
+        {
+            // check, if collectionId is non-existent for user
+            // returns NotFound HttpActionResult
+            Assert.Fail();
         }
     }
 }
