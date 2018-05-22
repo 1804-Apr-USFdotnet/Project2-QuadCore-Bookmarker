3c4e3f00599155051650ee7179cf905c08de724b
Billy <do.billy5@gmail.com>
2018-05-22 11:13:33 -0400

    made base api CollectionsBookmarksController

---
+++ Bookmarker.API/Bookmarker.API/Controllers/CollectionBookmarksController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Bookmarker.Models;
+using Bookmarker.Repositories;
+
+namespace Bookmarker.API.Controllers
+{
+    public class CollectionBookmarksController : ApiController
+    {
+        private readonly IDbContext _context;
+        private readonly Repository<Collection> _repo;
+
+        public CollectionBookmarksController()
+            : this(new BookmarkerContext())
+        {
+
+        }
+
+        public CollectionBookmarksController(IDbContext context)
+        {
+            _context = context;
+            _repo = new Repository<Collection>(_context);
+        }
+
+        [Route("collections/{collectionId}/bookmarks")]
+        public IHttpActionResult Get(Guid collectionId)
+        {
+            try
+            {
+                return InternalServerError();
+            }
+            catch (Exception ex)
+            {
+                //TODO: Log error
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("collections/{collectionId}/bookmarks/{index:int}")]
+        public IHttpActionResult GetByIndex(Guid collectionId, int index)
+        {
+            return InternalServerError();
+        }
+
+        [Route("collections/{collectionId}/bookmarks/{id}")]
+        public IHttpActionResult GetById(Guid collectionId, Guid id)
+        {
+            return InternalServerError();
+        }
+
+    }
+}
