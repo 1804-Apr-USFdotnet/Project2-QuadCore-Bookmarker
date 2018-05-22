c137ca7894a5ab0cea79f8e141dffce082dbab52
Billy <do.billy5@gmail.com>
2018-05-22 19:37:01 -0400

    finish implement methods

--- Bookmarker.API/Bookmarker.API/Controllers/CollectionBookmarksController.cs
+++ Bookmarker.API/Bookmarker.API/Controllers/CollectionBookmarksController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using Bookmarker.Models;
 using Bookmarker.Repositories;
 
+using Bookmarker.API.Models;
+
 namespace Bookmarker.API.Controllers
 {
     public class CollectionBookmarksController : ApiController
@@ -17,7 +19,6 @@ namespace Bookmarker.API.Controllers
         public CollectionBookmarksController()
             : this(new BookmarkerContext())
         {
-
         }
 
         public CollectionBookmarksController(IDbContext context)
@@ -31,7 +32,24 @@ namespace Bookmarker.API.Controllers
         {
             try
             {
-                return InternalServerError();
+                var collection = _repo.GetById(collectionId);
+                if (collection == null)
+                {
+                    return BadRequest($"No collection exists with ID ${collectionId}");
+                }
+
+                if (collection.Bookmarks == null)
+                {
+                    return NotFound();
+                }
+
+                List<BookmarkAPI> apiList = new List<BookmarkAPI>();
+                foreach(var bookmark in collection.Bookmarks)
+                {
+                    apiList.Add(new BookmarkAPI(bookmark));
+                }
+
+                return Ok(apiList);
             }
             catch (Exception ex)
             {
@@ -43,13 +61,53 @@ namespace Bookmarker.API.Controllers
         [Route("collections/{collectionId}/bookmarks/{index:int}")]
         public IHttpActionResult GetByIndex(Guid collectionId, int index)
         {
-            return InternalServerError();
+            try
+            {
+                var collection = _repo.GetById(collectionId);
+                if (collection == null || index <= 0)
+                {
+                    return BadRequest($"No collection exists with ID ${collectionId}");
+                }
+
+                if (collection.Bookmarks == null || collection.Bookmarks.Count() < index)
+                {
+                    return NotFound();
+                }
+
+                var bookmarkApi = new BookmarkAPI(collection.Bookmarks.ElementAt(index - 1));
+                return Ok(bookmarkApi);
+            }
+            catch (Exception ex)
+            {
+                //TODO: Log error
+                return InternalServerError(ex);
+            }
         }
 
         [Route("collections/{collectionId}/bookmarks/{id}")]
         public IHttpActionResult GetById(Guid collectionId, Guid id)
         {
-            return InternalServerError();
+            try
+            {
+                var collection = _repo.GetById(collectionId);
+                if (collection == null)
+                {
+                    return BadRequest($"No collection exists with ID ${collectionId}");
+                }
+
+                if (collection.Bookmarks == null || !collection.Bookmarks.Any(x => x.Id == id))
+                {
+                    return NotFound();
+                }
+
+                var bookmarkApi = new BookmarkAPI(collection.Bookmarks.FirstOrDefault(x => x.Id == id));
+                return Ok(bookmarkApi);
+            }
+            catch (Exception ex)
+            {
+                //TODO: Log error
+                return InternalServerError(ex);
+            }
         }
 
     }
