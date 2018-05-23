017aaa6402577a4c4333ac464988368d7613e63b
unknown <chaseharland@bellsouth.net>
2018-05-22 23:44:18 -0400

    Finished Sort Implementation
    progress on unit tests.

--- Bookmarker.API/Bookmarker.Logic/Library.cs
+++ Bookmarker.API/Bookmarker.Logic/Library.cs
@@ -11,61 +11,95 @@ namespace Bookmarker.Logic
     public static class Library
     {
         // Sort string format: "key1:asc,key2:desc,key3,key4"
-        private static Dictionary<string, Comparison<ABaseEntity>> entityComparisons =
+
+        // Sort terms:
+        // Date created, Date last modified
+        private static readonly Dictionary<string, Comparison<ABaseEntity>> entityComparisons =
             new Dictionary<string, Comparison<ABaseEntity>>
             {
-                { "created", (x,y) => x.Created.CompareTo(y.Created) },
-                { "created:asc", (x,y) => x.Created.CompareTo(y.Created) },
-                { "created:desc", (x,y) => y.Created.CompareTo(x.Created) },
-                { "modified", (x,y) => (x.Modified ?? new DateTime()).CompareTo(y.Modified ?? new DateTime()) },
-                { "modified:asc", (x,y) => (x.Modified ?? new DateTime()).CompareTo(y.Modified ?? new DateTime()) },
-                { "modified:desc", (x,y) => (y.Modified ?? new DateTime()).CompareTo(x.Modified ?? new DateTime()) }
+                { "created",        (x,y) => x.Created.CompareTo(y.Created) },
+                { "created:asc",    (x,y) => x.Created.CompareTo(y.Created) },
+                { "created:desc",   (x,y) => y.Created.CompareTo(x.Created) },
+                { "modified",       (x,y) => (x.Modified ?? new DateTime()).CompareTo(y.Modified ?? new DateTime()) },
+                { "modified:asc",   (x,y) => (x.Modified ?? new DateTime()).CompareTo(y.Modified ?? new DateTime()) },
+                { "modified:desc",  (x,y) => (y.Modified ?? new DateTime()).CompareTo(x.Modified ?? new DateTime()) }
             };
 
         // Sort terms:
-        // Name, Created, Edited
-        private static Dictionary<string, Comparison<User>> userComparisons =
+        // User name
+        private static readonly Dictionary<string, Comparison<User>> userComparisons =
             new Dictionary<string, Comparison<User>>
             {
-                { "name", (x,y) => x.Username.CompareTo(y.Username) },
-                { "name:asc", (x,y) => x.Username.CompareTo(y.Username) },
-                { "name:desc", (x,y) => y.Username.CompareTo(x.Username) },
+                { "name",       (x,y) => x.Username.CompareTo(y.Username) },
+                { "name:asc",   (x,y) => x.Username.CompareTo(y.Username) },
+                { "name:desc",  (x,y) => y.Username.CompareTo(x.Username) },
             };
 
         // Sort terms:
-        // Name, Rating, Owner, NumBookmarks, Private, OwnerName, Created, Edited
-        private static Dictionary<string, Comparison<Collection>> collectionComparisons =
+        // Name, Rating, Owner's Name, Number of Bookmarks, Private
+        private static readonly Dictionary<string, Comparison<Collection>> collectionComparisons =
             new Dictionary<string, Comparison<Collection>>
             {
-                //
+                { "name",       (x,y) => x.Name.CompareTo(y.Name) },
+                { "name:asc",   (x,y) => x.Name.CompareTo(y.Name) },
+                { "name:desc",  (x,y) => y.Name.CompareTo(x.Name) },
+
+                { "rating",     (x,y) => y.Rating.CompareTo(x.Rating) },
+                { "rating:asc", (x,y) => x.Rating.CompareTo(y.Rating) },
+                { "rating:desc",(x,y) => y.Rating.CompareTo(x.Rating) },
+
+                { "owner",      (x,y) => x.Owner.Username.CompareTo(y.Owner.Username) },
+                { "owner:asc",  (x,y) => x.Owner.Username.CompareTo(y.Owner.Username) },
+                { "owner:desc", (x,y) => y.Owner.Username.CompareTo(x.Owner.Username) },
+
+                { "bookmarks",      (x,y) => y.Bookmarks.Count().CompareTo(x.Bookmarks.Count()) },
+                { "bookmarks:asc",  (x,y) => x.Bookmarks.Count().CompareTo(y.Bookmarks.Count()) },
+                { "bookmarks:desc", (x,y) => y.Bookmarks.Count().CompareTo(x.Bookmarks.Count()) },
+
+                { "private",        (x,y) => x.Private.CompareTo(y.Private) },
+                { "private:asc",    (x,y) => x.Private.CompareTo(y.Private) },
+                { "private:desc",   (x,y) => x.Private.CompareTo(y.Private) }
             };
 
         // Sort terms:
-        // Title, URL, Collection, Rating, Created, Edited
-        private static Dictionary<string, Comparison<Bookmark>> bookmarkComparisons =
+        // Name, URL, Collection, Rating
+        private static readonly Dictionary<string, Comparison<Bookmark>> bookmarkComparisons =
             new Dictionary<string, Comparison<Bookmark>>
             {
-                //
+                { "name",       (x,y) => x.Name.CompareTo(y.Name) },
+                { "name:asc",   (x,y) => x.Name.CompareTo(y.Name) },
+                { "name:desc",  (x,y) => y.Name.CompareTo(x.Name) },
+
+                { "url",        (x,y) => x.URL.CompareTo(y.URL) },
+                { "url:asc",    (x,y) => x.URL.CompareTo(y.URL) },
+                { "url:desc",   (x,y) => y.URL.CompareTo(x.URL) },
+
+                { "collection",         (x,y) => x.Collection.Name.CompareTo(y.Collection.Name) },
+                { "collection:asc",     (x,y) => x.Collection.Name.CompareTo(y.Collection.Name) },
+                { "collection:desc",    (x,y) => y.Collection.Name.CompareTo(x.Collection.Name) },
+
+                { "rating",         (x,y) => y.Rating.CompareTo(x.Rating) },
+                { "rating:asc",     (x,y) => x.Rating.CompareTo(y.Rating) },
+                { "rating:desc",    (x,y) => y.Rating.CompareTo(x.Rating) }
             };
 
-        public static List<Bookmark> Sort(ref List<Bookmark> list, string sort)
+        public static void Sort(ref List<User> list, string sort)
         {
-            throw new NotImplementedException();
+            var comparitors = ParseComparisonListUsers(sort);
+            var aggregateComparitor = CombineComparisons(comparitors);
+            list.Sort(aggregateComparitor);
         }
-
-        public static List<Collection> Sort(ref List<Collection> list, string sort)
+        public static void Sort(ref List<Collection> list, string sort)
         {
-            throw new NotImplementedException();
+            var comparitors = ParseComparisonListCollections(sort);
+            var aggregateComparitor = CombineComparisons(comparitors);
+            list.Sort(aggregateComparitor);
         }
-
-        public static List<User> Sort(ref List<User> list, string sort)
+        public static void Sort(ref List<Bookmark> list, string sort)
         {
-            throw new NotImplementedException();
-        }
-
-        private static List<object> Sort(ref List<object> list, string sort)
-        {
-            throw new NotImplementedException();
+            var comparitors = ParseComparisonListBookmarks(sort);
+            var aggregateComparitor = CombineComparisons(comparitors);
+            list.Sort(aggregateComparitor);
         }
         
         private static Comparison<User>[] ParseComparisonListUsers(string sort)
@@ -88,7 +122,6 @@ namespace Bookmarker.Logic
 
             return list.ToArray();
         }
-
         private static Comparison<Collection>[] ParseComparisonListCollections(string sort)
         {
             List<Comparison<Collection>> list = new List<Comparison<Collection>();
@@ -109,12 +142,11 @@ namespace Bookmarker.Logic
 
             return list.ToArray();
         }
-
         private static Comparison<Bookmark>[] ParseComparisonListBookmarks(string sort)
         {
             List<Comparison<Bookmark>> list = new List<Comparison<Bookmark>();
 
-            string[] terms = sort.Split(',');
+            string[] terms = sort.ToLower().Split(',');
 
             foreach (string term in terms)
             {
--- Bookmarker.API/Bookmarker.Test/TestLogicSort.cs
+++ Bookmarker.API/Bookmarker.Test/TestLogicSort.cs
@@ -106,20 +106,105 @@ namespace Bookmarker.Test
         //    User a = new User() { Modified = new DateTime(2000, 1, 1) };
         //}
 
+
+        private static readonly User u1 = new User() { Username = "aaa", Created = new DateTime(1, 1, 1), Modified = new DateTime(3, 3, 3) };
+        private static readonly User u2 = new User() { Username = "aa", Created = new DateTime(2, 2, 2), Modified = new DateTime(2, 2, 2) };
+        private static readonly User u3 = new User() { Username = "bbb", Created = new DateTime(1, 1, 2), Modified = new DateTime(5, 5, 5) };
+        private static readonly User u4 = new User() { Username = "ccc", Created = new DateTime(3, 3, 3), Modified = new DateTime(1, 1, 1) };
+        private static readonly User u5 = new User() { Username = "ddd", Created = new DateTime(4, 4, 4), Modified = new DateTime(4, 4, 4) };
+        
+        private static readonly Collection c1 = new Collection() { Name = "bbb", Owner = u1, Private = true };
+        private static readonly Collection c2 = new Collection() { Name = "aaa", Owner = u2, Private = true };
+        private static readonly Collection c3 = new Collection() { Name = "ccc", Owner = u3, Private = true };
+        private static readonly Collection c4 = new Collection() { Name = "ddd", Owner = u4, Private = true };
+        private static readonly Collection c5 = new Collection() { Name = "eee", Owner = u5, Private = true };
+        private static readonly Collection c6 = new Collection() { Name = "fff", Owner = u1, Private = true };
+        
+        private static readonly Bookmark b1 = new Bookmark() { Name = "bbb", URL = "jjj", Collection = c1 };
+        private static readonly Bookmark b2 = new Bookmark() { Name = "aaa", URL = "iii", Collection = c2 };
+        private static readonly Bookmark b3 = new Bookmark() { Name = "ccc", URL = "hhh", Collection = c3 };
+        private static readonly Bookmark b4 = new Bookmark() { Name = "ddd", URL = "ggg", Collection = c4 };
+        private static readonly Bookmark b5 = new Bookmark() { Name = "eee", URL = "fff", Collection = c5 };
+        private static readonly Bookmark b6 = new Bookmark() { Name = "fff", URL = "eee", Collection = c6 };
+        private static readonly Bookmark b7 = new Bookmark() { Name = "ggg", URL = "ddd", Collection = c1 };
+        private static readonly Bookmark b8 = new Bookmark() { Name = "hhh", URL = "ccc", Collection = c2 };
+        private static readonly Bookmark b9 = new Bookmark() { Name = "iii", URL = "aaa", Collection = c2 };
+        private static readonly Bookmark b0 = new Bookmark() { Name = "jjj", URL = "bbb", Collection = c4 };
+        
+        private readonly List<User> testUsers;
+        private readonly List<Collection> testCollections;
+        private readonly List<Bookmark> testBookmarks;
+
+        public TestLogicSort()
+        {
+            testUsers = new List<User>() { u1, u2, u3, u4, u5 };
+            testCollections = new List<Collection>() { c1, c2, c3, c4, c5, c6 };
+            testBookmarks = new List<Bookmark>() { b1, b2, b3, b4, b5, b6, b7, b8, b9, b0 };
+
+            u1.Collections = new List<Collection>() { c1, c6 };
+            u2.Collections = new List<Collection>() { c2 };
+            u3.Collections = new List<Collection>() { c3 };
+            u4.Collections = new List<Collection>() { c4 };
+            u5.Collections = new List<Collection>() { c5 };
+
+            c1.Bookmarks = new List<Bookmark>() { b1, b7 };
+            c2.Bookmarks = new List<Bookmark>() { b2, b8, b9 };
+            c3.Bookmarks = new List<Bookmark>() { b3 };
+            c4.Bookmarks = new List<Bookmark>() { b4, b0 };
+            c5.Bookmarks = new List<Bookmark>() { b5 };
+            c6.Bookmarks = new List<Bookmark>() { b6 };
+
+            c1.ThumbUp(); c1.ThumbUp(); c1.ThumbUp();
+            c2.ThumbUp();
+            c3.ThumbUp(); c3.ThumbUp();
+            c4.ThumbUp(); c4.ThumbUp();
+            c5.ThumbUp(); c5.ThumbUp(); c5.ThumbUp(); c5.ThumbUp();
+            c6.ThumbUp();
+
+            b1.ThumbUp(); b1.ThumbUp(); b1.ThumbUp();
+            b2.ThumbUp();
+            b3.ThumbUp();
+            b4.ThumbUp(); b4.ThumbUp(); b4.ThumbUp(); b4.ThumbUp();
+            b5.ThumbUp();
+            b6.ThumbUp();
+            b7.ThumbUp(); b7.ThumbUp(); b7.ThumbUp();
+            b8.ThumbUp();
+            b9.ThumbUp(); b9.ThumbUp();
+            b0.ThumbUp();
+        }
+
         [TestMethod]
         public void TestSortUser()
         {
-            throw new NotImplementedException();
+            string sort = "name";
+            List<User> actual = new List<User>(testUsers);
+            List<User> expected = new List<User> { u2, u1, u3, u4, u5};
+
+            Library.Sort(ref actual, sort);
+
+            Assert.AreEqual(actual, expected);
         }
         [TestMethod]
         public void TestSortCollection()
         {
-            throw new NotImplementedException();
+            string sort = "owner:desc";
+            List<Collection> actual = new List<Collection>(testCollections);
+            List<Collection> expected = new List<Collection>() { c5, c4, c3, c1, c6, c2};
+
+            Library.Sort(ref actual, sort);
+
+            Assert.AreEqual(actual, expected);
         }
         [TestMethod]
         public void TestSortBookmark()
         {
-            throw new NotImplementedException();
+            string sort = "name:asc";
+            List<Bookmark> actual = new List<Bookmark>(testBookmarks);
+            List<Bookmark> expected = new List<Bookmark>() { b2, b1, b3, b4, b5, b6, b7, b8, b9, b0 };
+
+            Library.Sort(ref actual, sort);
+
+            Assert.AreEqual(actual, expected);
         }
         [TestMethod]
         public void TestSortAsc()
