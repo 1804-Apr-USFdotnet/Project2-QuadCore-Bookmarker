b0954788d518898cbacf0fc78a159710aaff3d3b
Timothy Hayes <tjhayes011@gmail.com>
2018-05-22 22:22:48 -0400

    Delete unused scaffolded views

--- Bookmarker.MVC/Bookmarker.MVC/Controllers/HomeController.cs
+++ Bookmarker.MVC/Bookmarker.MVC/Controllers/HomeController.cs
@@ -12,36 +12,12 @@ namespace Bookmarker.MVC.Controllers
 {
     public class HomeController : AServiceController
     {
-        public ActionResult Index()
-        {
-            /* TODO: Check if user is logged in
-             * If yes, send to user-specific home page
-             * If no,  send to general home page
-             */
-
-            return View(new List<CollectionViewModel>());
-        }
-
         public ActionResult Home()
         {
             /* TODO: User-specific home page
              * if user is not authenticated, give them an error screen
              * otherwise, show their personal page
              */
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
@@ -72,7 +48,6 @@ namespace Bookmarker.MVC.Controllers
             {
                 return View(await apiResponse.Content.ReadAsAsync<IEnumerable<AccountViewModel>>());
             }
-
         }
     }
 }
